72b698593f53300a1febbfeed61e20e421d1fd19
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2023-11-03 00:52:31 +0300

    добавляет тесты

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,38 @@
+import { classNames } from '.';
+
+describe('classNames', () => {
+  const expected = 'someClass';
+
+  test('только с одним первым параметром', () => {
+    expect(classNames('someClass'))
+      .toBe(expected);
+  });
+
+  test('дополнительными классами', () => {
+    const expected = 'someClass class1 class2';
+
+    expect(classNames('someClass', ['class1', 'class2']))
+      .toBe(expected);
+  });
+
+  test('модами hovered scrollable', () => {
+    const expected = 'someClass class1 class2 hovered scrollable';
+
+    expect(classNames('someClass', ['class1', 'class2'], { hovered: true, scrollable: true }))
+      .toBe(expected);
+  });
+
+  test('модами hovered', () => {
+    const expected = 'someClass class1 class2 hovered';
+
+    expect(classNames('someClass', ['class1', 'class2'], { hovered: true, scrollable: false }))
+      .toBe(expected);
+  });
+
+  test('модами scrollable вместе с hovered undefined', () => {
+    const expected = 'someClass class1 class2 scrollable';
+
+    expect(classNames('someClass', ['class1', 'class2'], { hovered: undefined, scrollable: true }))
+      .toBe(expected);
+  });
+});
--- src/shared/lib/classNames/index.ts
+++ src/shared/lib/classNames/index.ts
@@ -1,17 +1,17 @@
-type TMods = Record<string, boolean | string>
+type TMods = Record<string, boolean | string | undefined>
 
-function classNames (
+const classNames = (
   cls: string,
-  additional: [string | undefined],
-  mods: TMods = {}
-): string {
+  additional?: Array<string | undefined>,
+  mods?: TMods
+): string => {
   return [
     cls,
-    ...additional,
-    ...Object.entries(mods)
+    ...additional ?? [],
+    ...Object.entries(mods ?? {})
       .filter(([_, value]) => Boolean(value))
       .map(([cls]) => cls)
   ].join(' ');
-}
+};
 
 export { classNames };
